38f5b382d2f010fb08c38c80dd9c422aca5a1f52
Leland Crawford <lelandscrawford@gmail.com>
2021-10-07 05:27:17 -0400

    still not finding the api.urls module

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlspatterns = [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('api.urls'))
+    path('', include('frontend.urls'))
 ]
