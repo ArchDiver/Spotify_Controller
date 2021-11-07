84e1504a1af35e0e956a63970a10891351541371
Leland Crawford <lelandscrawford@gmail.com>
2021-11-06 20:04:07 -0400

    fixed misnamed module

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, "./static/frontend"),
         filename: "[name].js",
     },
-    modle: {
+    module: {
         rules: [
             {
                 test: /\.js$/,
