f52610c60f337f5558578cf7353f43ff434d0bbb
Leland Crawford <lelandscrawford@gmail.com>
2021-11-11 07:16:04 -0500

    trying to get test to show up

--- music_controller/frontend/src/compontents/App.js
+++ music_controller/frontend/src/compontents/App.js
@@ -7,11 +7,12 @@ export default class App extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <h1> Testing React Code </h1> 
-            </div>
-        )
+        return <h1>Testing React Code</h1>;
+        // return (
+        //     <div>
+        //         <h1> Testing React Code </h1> 
+        //     </div>
+        // )
     }
 }
 
